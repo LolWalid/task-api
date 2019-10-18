b19cd3a6723ccc0c62587d2a23380bfe116ca1ad
Walid <walid.seoudi@gmail.com>
2019-10-18 23:26:42 +0200

    signup_and_login: move methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,13 @@ module API::V1
   class UsersController < ApplicationController
     before_action :authenticate!, only: :refresh_token
 
-    def refresh_token
-      token = JSONWebToken.encode(user_id: current_user.id)
-      render json: { token: token, expire: User::JWT_DURATION.from_now }, status: :ok
+    def sign_up
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
     end
 
     def login
@@ -17,13 +21,9 @@ module API::V1
       end
     end
 
-    def sign_up
-      @user = User.new(user_params)
-      if @user.save
-        render json: @user, status: :created
-      else
-        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-      end
+    def refresh_token
+      token = JSONWebToken.encode(user_id: current_user.id)
+      render json: { token: token, expire: User::JWT_DURATION.from_now }, status: :ok
     end
 
     private
